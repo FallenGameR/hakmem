be61dd8bc3a5e26a8a6fbf3fecc0529f2a29c556
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2023-01-18 12:15:28 -0800

    more operations

--- ch02/playground_rust/src/main.rs
+++ ch02/playground_rust/src/main.rs
@@ -4,26 +4,22 @@ fn main() {
     println!("turn_off_rightmost_1bit => x & (x - 1)");
     let f = turn_off_rightmost_1bit;
     apply(0b_1010_1000, 0b_0000_0000, f);
-    println!("pow2 | all zeros");
-    zero_test(0b_0010_0000, f);
-    zero_test(0b_0000_0000, f);
-    zero_test(0b_0000_0101, f);
-
-    println!();
-    println!("turn_on_rightmost_0bit => x | (x + 1)");
-    let f = turn_on_rightmost_0bit;
-    apply(0b_1010_0111, 0b_1111_1111, f);
+    zero_test(0b_0010_0000, f, "pow2");
+    zero_test(0b_0000_0000, f, "all zeros");
 
     println!();
     println!("turn_off_rightaligned_1bit_group => x & (x + 1)");
     let f = turn_off_rightaligned_1bit_group;
     let x = apply(0b_1010_0111, 0b_1010_0000, f);
-    println!(" {:08b} same", f(x));
-    println!("pow2-1 | all zeros | all ones");
-    zero_test(0b_0011_1111, f);
-    zero_test(0b_0000_0000, f);
-    zero_test(0b_1111_1111, f);
-    zero_test(0b_1010_0111, f);
+    println!(" {:08b} - same", f(x));
+    zero_test(0b_0011_1111, f, "pow2 - 1");
+    zero_test(0b_0000_0000, f, "all zeros");
+    zero_test(0b_1111_1111, f, "all ones");
+
+    println!();
+    println!("turn_on_rightmost_0bit => x | (x + 1)");
+    let f = turn_on_rightmost_0bit;
+    apply(0b_1010_0111, 0b_1111_1111, f);
 
     println!();
     println!("turn_on_rightaligned_0bit_group => x | (x - 1)");
@@ -32,22 +28,42 @@ fn main() {
     println!(" {:08b} same", f(x));
 
     println!();
-    println!("mask0_rightmost_1bit => !x | (x-1)");
+    println!("mask0_rightmost_1bit => !x | (x - 1)");
     let f = mask0_rightmost_1bit;
-    apply(0b_1010_1000, 0b_1111_1110, f);
+    apply(0b_1010_1000, 0b_1111_1101, f);
     println!("all ones if there is none");
     let x = 0b_0000_0000;
     println!(" {:08b}", x);
     println!(" {:08b}", f(x));
 
     println!();
-    println!("mask1_rightmost_0bit => !x & (x+1)");
+    println!("mask1_rightmost_0bit => !x & (x + 1)");
     let f = mask1_rightmost_0bit;
-    apply(0b_1010_0111, 0b_0000_0001, f);
+    apply(0b_1010_0111, 0b_0000_0010, f);
     println!("all zeros if there is none");
     let x = 0b_1111_1111;
     println!(" {:08b}", x);
     println!(" {:08b}", f(x));
+
+    println!();
+    println!("mask1_rightmost_1bit => x & -x");
+    let f = mask1_rightmost_1bit;
+    let x = apply(0b_1010_1000, 0b_0000_1000, f);
+    println!(" {:08b} - same", f(x));
+    println!("all zeros if there is none");
+    let x = 0b_0000_0000;
+    println!(" {:08b}", x);
+    println!(" {:08b}", f(x));
+
+    println!();
+    println!("mask1_rightaligned_0bit_group => !x & (x - 1)");
+    let f = mask1_rightaligned_0bit_group;
+    apply(0b_1010_1000, 0b_1111_1111, f);
+
+    println!();
+    println!("mask0_rightaligned_1bit_group => !x | (x + 1)");
+    let f = mask0_rightaligned_1bit_group;
+    apply(0b_1010_0111, 0b_0000_0000, f);
 }
 
 fn apply<F>(start: u8, stop: u8, f: F) -> u8
@@ -59,6 +75,15 @@ where F: Fn(u8) -> u8 {
         println!(" {:08b}", x);
     }
     x
+}
+
+fn zero_test<F>(x: u8, f: F, info: &str) where F: Fn(u8) -> u8 {
+    if f(x) == 0 {
+        println!(" {:08b} -> {}", x, info)
+    }
+    else{
+        println!(" {:08b} -> NOT ZERO, no {}", x, info)
+    }
 }
 
 /// 0101 1000 => 0101 0000
@@ -102,6 +127,33 @@ fn mask1_rightmost_0bit(x: u8) -> u8 {
     !x & x.wrapping_add(1)
 }
 
-fn zero_test<F>(x: u8, f: F) where F: Fn(u8) -> u8 {
-    println!(" {:08b} -> {}", x, f(x) == 0)
+/// 0101 1000: x => 0000 1000
+/// 1010 0111: !x
+/// 1010 1000: -x = !x + 1; x + (-x) = 0
+fn mask1_rightmost_1bit(x: u8) -> u8 {
+    let x = x as i8;
+    let result = x & -x;
+    result as u8
+}
+
+/// 0101 1000 => 0000 0111
+/// 0101 0111: x-1
+/// 1010 0111: !x
+fn mask1_rightaligned_0bit_group(x: u8) -> u8 {
+    // !x & (x-1)
+    !x & x.wrapping_sub(1)
+
+    // Alternative formulas that lack instruction level parrallelizm:
+    // !(x | -x)
+    // (x & -x) - 1
+}
+
+
+
+/// 0101 0111: x => 1111 1000
+/// 1010 1000: !x
+/// 0101 1000: x+1
+fn mask0_rightaligned_1bit_group(x: u8) -> u8 {
+    // !x | (x+1)
+    !x | x.wrapping_add(1)
 }
